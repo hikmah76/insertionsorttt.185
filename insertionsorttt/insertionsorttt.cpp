7b30838ff2b09cff061c6594784f691c125f1950
nur hikmah <nur.hikmah190504@gmail.com>
2023-03-24 12:15:02 +0700

    menuat inputan memanggil variable membuat kondisi

--- insertionsorttt/insertionsorttt.cpp
+++ insertionsorttt/insertionsorttt.cpp
@@ -4,3 +4,18 @@ using namespace std;
 int arr[20];                    // Membuat Array dengan panjang data 20
 int n;                          // Membuat Variable inputan n
                           
+void input() {                  // Procedure input 
+    while (true)
+    {
+        cout << "Masukan Jumlah Data pada Array : ";    // Membuat inputan jumlah element Array
+        cin >> n;                                       // memanggil variable inputan n
+
+        if (n <= 20) {                                  // Membuat Kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen. \n";    // Menampilkan pesan Jika data lebih dari 20
+        }
+    }
+
