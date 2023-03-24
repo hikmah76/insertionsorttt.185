9e3f839a3ad5e83403d7ac574cfa9bf31f80b5ef
nur hikmah <nur.hikmah190504@gmail.com>
2023-03-24 12:30:51 +0700

    Menggunakan perulangan menginputkan nilai Menyimpan nilai

--- insertionsorttt/insertionsorttt.cpp
+++ insertionsorttt/insertionsorttt.cpp
@@ -21,10 +21,12 @@ void input() {                  // Procedure input
     cout << endl;                                   // Membuat jarak per baris program 
     cout << "======================" << endl;       // Membuat tampilan susunan data element array
     cout << "Masukkan Element Array" << endl;
-    cout << "======================" << endl; 
+    cout << "======================" << endl;
 
 
     for (int i = 0; i < n; i++)                     // Menggunakan perulangan for untuk menyimpan data array
     {
         cout << "Data ke-" << (i + 1) << ": ";      // Masukkan atau menginputkan nilai data n
-        cin >> arr[i];                              // Menyimpan nilai data n 
+        cin >> arr[i];                              // Menyimpan nilai data n kedalam array arr
+    }
+}
